6680fc8861a779ab4c04e38ba358cfa6d872c114
prop-ceko <128106173+prop-ceko@users.noreply.github.com>
2023-05-04 20:45:20 +0000

    Proyecto creado

---
+++ app/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+import mkcert from 'vite-plugin-mkcert'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: { https: true },
+  plugins: [react(), eslint(), mkcert()],
+})
